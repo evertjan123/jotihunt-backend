86bc3361d9c93ad0fd74f0ead591f6928383232c
Evertjan Leusink <evertjan@dharma.nl>
2023-10-20 01:10:49 +0200

    added HUNTS

--- app/Http/Controllers/HuntsController.php
+++ app/Http/Controllers/HuntsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Hunter;
 use App\Models\Hunts;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class HuntsController extends Controller
 {
